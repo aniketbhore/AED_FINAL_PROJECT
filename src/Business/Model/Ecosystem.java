c473600bddab9f10593229418291451620cad920
aniketbhore <bhore.a@northeastern.edu>
2022-12-09 16:02:53 -0500

    Model Setup: Added Ecosystem, Organisation, SystemConfiguration. Also Database Setup
    Model Setup: Added Ecosystem, Organisation, SystemConfiguration. Also Database Setup.

---
+++ src/Business/Model/Ecosystem.java
@@ -0,0 +1,125 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Business.Model;
+
+import java.util.ArrayList;
+import javax.management.relation.Role;
+
+/**
+ *
+ * @author aniketbhore
+ */
+public class Ecosystem extends Organisation{
+    private static Ecosystem business;
+    private PatientDirectory patientDirectory;
+    private DoctorDirectory doctorDirectory;
+    private AmbulanceDirectory ambulanceDirectory;
+    private BloodBank bloodBank;
+    private Lab lab;
+    private Pharmacy pharmacy;
+    private InsuranceDirectory insuranceDirectory;
+    private PoliceDepartment policeRecords;
+    
+    public static Ecosystem getInstance()
+    {
+        if(business==null)
+        {
+            business=new Ecosystem();
+        }
+        
+        return business;
+    }
+
+    public static Ecosystem getBusiness() {
+        return business;
+    }
+
+    public static void setBusiness(Ecosystem business) {
+        Ecosystem.business = business;
+    }
+
+    public PatientDirectory getPatientDirectory() {
+        return patientDirectory = (patientDirectory == null) ? new PatientDirectory() : patientDirectory;
+    }
+
+    public void setPatientDirectory(PatientDirectory patientDirectory) {
+        this.patientDirectory = patientDirectory;
+    }
+
+    public DoctorDirectory getDoctorDirectory() {
+        return doctorDirectory = (doctorDirectory == null) ? new DoctorDirectory() : doctorDirectory;
+    }
+
+    public void setDoctorDirectory(DoctorDirectory doctorDirectory) {
+        this.doctorDirectory = doctorDirectory;
+    }
+
+    public AmbulanceDirectory getAmbulanceDirectory() {
+        return ambulanceDirectory = (ambulanceDirectory == null) ? new AmbulanceDirectory() : ambulanceDirectory;
+    }
+
+    public void setAmbulanceDirectory(AmbulanceDirectory ambulanceDirectory) {
+        this.ambulanceDirectory = ambulanceDirectory;
+    }
+
+    public BloodBank getBloodBank() {
+        return bloodBank = (bloodBank == null) ? new BloodBank() : bloodBank;
+    }
+
+    public void setBloodBank(BloodBank bloodBank) {
+        this.bloodBank = bloodBank;
+    }
+
+    public Lab getLab() {
+        return lab = (lab == null)? new Lab() : lab;
+    }
+
+    public void setLab(Lab lab) {
+        this.lab = lab;
+    }
+
+    public Pharmacy getPharmacy() {
+        return pharmacy = (pharmacy == null)? new Pharmacy() : pharmacy;
+    }
+
+    public void setPharmacy(Pharmacy pharmacy) {
+        this.pharmacy = pharmacy;
+    }
+
+    public InsuranceDirectory getInsuranceDirectory() {
+        return insuranceDirectory = (insuranceDirectory == null)? new InsuranceDirectory() : insuranceDirectory;
+    }
+
+    public void setInsuranceDirectory(InsuranceDirectory insuranceDirectory) {
+        this.insuranceDirectory = insuranceDirectory;
+    }
+
+    public PoliceDepartment getPoliceRecords() {
+        return policeRecords = (policeRecords == null)? new PoliceDepartment() : policeRecords;
+    }
+
+    public void setPoliceRecords(PoliceDepartment policeRecords) {
+        this.policeRecords = policeRecords;
+    }
+
+
+    
+    @Override
+    public ArrayList<Role> getSupportedRole() {
+        ArrayList<Role> roleList=new ArrayList<>();
+        roleList.add(new Administrator());
+        return roleList;
+    }
+    private Ecosystem()
+    {
+        super(null);
+    }
+
+    
+    public boolean checkIfUserIsUnique(String userName)
+    {
+       return false;
+    }
+}
---
+++ src/BusinessModel/DB4OUtil/DB4OUtil.java
@@ -0,0 +1,77 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package BusinessModel.DB4OUtil;
+
+import Business.Model.Ecosystem;
+import Business.Model.SystemConfiguration;
+import com.db4o.Db4oEmbedded;
+import com.db4o.ObjectContainer;
+import com.db4o.ObjectSet;
+import com.db4o.config.EmbeddedConfiguration;
+import com.db4o.ta.TransparentPersistenceSupport;
+import java.nio.file.Paths;
+
+/**
+ *
+ * @author aniketbhore
+ */
+public class DB4OUtil {
+    private static final String FILENAME = Paths.get("Databank.db4o").toAbsolutePath().toString();
+
+    private static DB4OUtil dB4OUtil;
+
+    public synchronized static DB4OUtil getInstance() {
+        if (dB4OUtil == null) 
+        {
+            dB4OUtil = new DB4OUtil();
+        }
+        return dB4OUtil;
+    }
+
+    protected synchronized static void shutdown(ObjectContainer conn) {
+        if (conn != null) {
+            conn.close();
+        }
+    }
+
+    private ObjectContainer createConnection() {
+        try {
+            EmbeddedConfiguration config = Db4oEmbedded.newConfiguration();
+            System.out.println("file name before create connection---"+FILENAME);
+            ObjectContainer database = Db4oEmbedded.openFile(config, FILENAME);
+            System.out.println("database ---"+database);
+            config.common().add(new TransparentPersistenceSupport());
+            config.common().activationDepth(Integer.MAX_VALUE);
+            config.common().updateDepth(Integer.MAX_VALUE);
+            config.common().objectClass(Ecosystem.class).cascadeOnUpdate(true);
+            return database;
+        } catch (Exception ex) {
+            System.out.print("exception ----"+ex.getMessage());
+        }
+        return null;
+    }
+
+    public synchronized void storeSystem(Ecosystem system) {
+        ObjectContainer conn = createConnection();
+        conn.store(system);
+        conn.commit();
+        conn.close();
+    }
+
+    public Ecosystem retrieveSystem() {
+        ObjectContainer conn = createConnection();
+        System.out.println("connection ---"+conn);
+        ObjectSet<Ecosystem> systems = conn.query(Ecosystem.class);
+       
+        Ecosystem system;
+        if (systems.isEmpty()) {
+            system = SystemConfiguration.configure();
+        } else {
+            system = systems.get(systems.size() - 1);
+        }
+        conn.close();
+        return system;
+    }
+}
